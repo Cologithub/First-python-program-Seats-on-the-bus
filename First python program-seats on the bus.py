cd885c49fcaff7db55f6ad8669829420d0faac99
Cologithub <130111127+Cologithub@users.noreply.github.com>
2023-06-02 08:06:29 -0600

    Add files via upload

---
+++ First python program-seats on the bus.py
@@ -0,0 +1,35 @@
+print("My reintroduction to python begins today on 01/06/23.")
+
+print("We were tought python in school, but it was too advanced for me at the time.")
+
+print("After learning HTML, CSS and JAVASCRIPT basics, my overall knowledge of how to code has dramatically improved since then.")
+
+print("God wiling, this will be a fun python journey and we'll make the best of it:)")
+
+#Here we have a mini program.
+#A group of school children want to go on a field trip.
+#The bus can take 15 students.
+#Normally on field trips, there's a chance that not everyone will show up.
+#So, depending on the amount of students that show up, a message will be displayed by the teacher.
+
+seats=15
+
+print ("how many seats are there?")
+print("There are "+ str(seats) + " on the bus.")
+
+print("How many kids will be on the bus today?")
+print("There will be 7 children present on the bus today.")
+
+the_children_present=1
+
+if the_children_present==1:
+   print("NOPE, WE CANT GO WITH ONLY ONE CHILD LOOL")
+
+if the_children_present==7:
+ print ("Nope, the number is to little to take on the field trip:(")
+
+
+if the_children_present>=7:
+    print("YHUP! OKAY WE CAN GO ON THE FIED TRIP:)")
+
+    
